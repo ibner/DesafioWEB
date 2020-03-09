9695be2d243eb331758ad9bdc99288b80a076204
ibner <ibner@Ibner-PC>
2020-03-09 19:23:36 -0300

    Commit do projeto ProjetoWeb

---
+++ projetoWeb/src/main/java/br/com/ibner/projetoweb/model/Produto.java
@@ -0,0 +1,61 @@
+package br.com.ibner.projetoweb.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String descricao;
+	private String nome;
+	private int quantidade;
+
+	public Produto() {
+		
+	}
+	
+	public Produto(String descricao, String nome, int quantidade) {
+		this.descricao=descricao;
+		this.nome=nome;
+		this.quantidade=quantidade;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
